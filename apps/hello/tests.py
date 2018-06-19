1ec0e70884f6143f915e28ccbb5394555644ab3b
misha <mishaelitzem2@rambler.ru>
2018-06-19 21:38:10 +0300

    ticket:1 test for view tests fail

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 4)
+class ContactsViewTests(TestCase):
+    def setUp(self):
+        """ Every test needs a client"""
+        self.client = Client()
+
+    def test_start_page_view(self):
+        """Issue a GET request"""
+        response = self.client.get('/')
+
+        """Check that the response is 200 OK"""
+        self.assertEqual(response.status_code, 200)
+
+        """Check that the response template"""
+        self.assertEqual(response.template_name, ['contacts.html', ])
+
+        """Check that the response function`s name is 'contacts'"""
+        self.assertEqual(response.get.im_func.func_name, 'contacts')
+
+
