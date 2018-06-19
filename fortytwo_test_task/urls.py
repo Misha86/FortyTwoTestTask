d955eebbe4ca8dc92d8c7cb8fc4dfce1bad3ac4b
misha <mishaelitzem2@rambler.ru>
2018-06-19 20:16:40 +0300

    ticket:1 turn html mockup into django template
    	новый файл:    apps/hello/templates/contacts.html
    	изменено:      apps/hello/tests.py
    	изменено:      fortytwo_test_task/settings/common.py
    	изменено:      fortytwo_test_task/urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +12,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', TemplateView.as_view(template_name='contacts.html')),
 )
